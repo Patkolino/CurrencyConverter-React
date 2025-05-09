06f41d08277e7594ce1189550b3f4fa41a741292
Patrycja K <patrycjakostyniak@gmail.com>
2025-05-09 16:41:39 +0200

    updated result, removed the previously set &nbsp that was reserving space, added min height via css to avoid the "jump" if the result is calculated and shown

--- src/App/Form/Result/result.js
+++ src/App/Form/Result/result.js
@@ -1,12 +1,10 @@
 const Result = ({ result }) => (
     <p className="form__paragraph">
-        {result ?
+        {result &&
             <>
                 For <strong>{result.inputAmount} {result.from}</strong> you will receive{" "}
                 <strong>{result.outputAmount} {result.to}</strong>.
             </>
-            :
-            <>&nbsp;</>
         }
     </p>
 );
